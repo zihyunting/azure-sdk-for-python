9bd4451d277d60f8803fc603ba2ec3710989d790
SDKAuto <sdkautomation@microsoft.com>
2023-10-03 07:20:35 +0000

    CodeGen from PR 25994 in Azure/azure-rest-api-specs Merge d8b4c5ad5d4813bb8bbe055338a731f9f31dff86 into c67016198d67ef0d833f12fe867b1adbad513315

--- sdk/cdn/azure-mgmt-cdn/azure/mgmt/cdn/models/_cdn_management_client_enums.py
+++ sdk/cdn/azure-mgmt-cdn/azure/mgmt/cdn/models/_cdn_management_client_enums.py
@@ -106,6 +106,13 @@ class CacheType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     ALL = "All"
 
 
+class CanMigrateDefaultSku(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """Recommended sku for the migration."""
+
+    STANDARD_AZURE_FRONT_DOOR = "Standard_AzureFrontDoor"
+    PREMIUM_AZURE_FRONT_DOOR = "Premium_AzureFrontDoor"
+
+
 class CdnCertificateSourceParametersTypeName(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """CdnCertificateSourceParametersTypeName."""
 
@@ -478,6 +485,17 @@ class ManagedRuleEnabledState(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     ENABLED = "Enabled"
 
 
+class ManagedServiceIdentityType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """Type of managed service identity (where both SystemAssigned and UserAssigned types are
+    allowed).
+    """
+
+    NONE = "None"
+    SYSTEM_ASSIGNED = "SystemAssigned"
+    USER_ASSIGNED = "UserAssigned"
+    SYSTEM_ASSIGNED_USER_ASSIGNED = "SystemAssigned, UserAssigned"
+
+
 class MatchProcessingBehavior(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """If this rule is a match should the rules engine continue running the remaining rules or stop.
     If not present, defaults to Continue.
@@ -485,6 +503,7 @@ class MatchProcessingBehavior(str, Enum, metaclass=CaseInsensitiveEnumMeta):
 
     CONTINUE = "Continue"
     STOP = "Stop"
+    CONTINUE_ENUM = "Continue"
 
 
 class MatchVariable(str, Enum, metaclass=CaseInsensitiveEnumMeta):
@@ -511,16 +530,16 @@ class MatchVariable(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     SSL_PROTOCOL = "SslProtocol"
 
 
-class MetricsResponseGranularity(str, Enum, metaclass=CaseInsensitiveEnumMeta):
-    """MetricsResponseGranularity."""
+class MetricsGranularity(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """MetricsGranularity."""
 
     PT5_M = "PT5M"
     PT1_H = "PT1H"
     P1_D = "P1D"
 
 
-class MetricsResponseSeriesItemUnit(str, Enum, metaclass=CaseInsensitiveEnumMeta):
-    """MetricsResponseSeriesItemUnit."""
+class MetricsSeriesUnit(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """MetricsSeriesUnit."""
 
     COUNT = "count"
     BYTES = "bytes"
@@ -707,6 +726,18 @@ class ProfileResourceState(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     CREATING = "Creating"
     ACTIVE = "Active"
     DELETING = "Deleting"
+    DISABLED = "Disabled"
+    MIGRATING = "Migrating"
+    MIGRATED = "Migrated"
+    PENDING_MIGRATION_COMMIT = "PendingMigrationCommit"
+    COMMITTING_MIGRATION = "CommittingMigration"
+    ABORTING_MIGRATION = "AbortingMigration"
+
+
+class ProfileScrubbingState(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """State of the log scrub config. Default value is Enabled."""
+
+    ENABLED = "Enabled"
     DISABLED = "Disabled"
 
 
@@ -953,6 +984,27 @@ class RuleQueryStringCachingBehavior(str, Enum, metaclass=CaseInsensitiveEnumMet
     USE_QUERY_STRING = "UseQueryString"
     IGNORE_SPECIFIED_QUERY_STRINGS = "IgnoreSpecifiedQueryStrings"
     INCLUDE_SPECIFIED_QUERY_STRINGS = "IncludeSpecifiedQueryStrings"
+
+
+class ScrubbingRuleEntryMatchOperator(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """Comparison type to use for matching with the variable value in log."""
+
+    EQUALS_ANY = "EqualsAny"
+
+
+class ScrubbingRuleEntryMatchVariable(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """The variable to be scrubbed from the logs."""
+
+    REQUEST_IP_ADDRESS = "RequestIPAddress"
+    REQUEST_URI = "RequestUri"
+    QUERY_STRING_ARG_NAMES = "QueryStringArgNames"
+
+
+class ScrubbingRuleEntryState(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """Defines the state of log scrubbing rule. Default value is Enabled."""
+
+    ENABLED = "Enabled"
+    DISABLED = "Disabled"
 
 
 class SecretType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
@@ -1216,16 +1268,16 @@ class WafMetric(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     CLIENT_REQUEST_COUNT = "clientRequestCount"
 
 
-class WafMetricsResponseGranularity(str, Enum, metaclass=CaseInsensitiveEnumMeta):
-    """WafMetricsResponseGranularity."""
+class WafMetricsGranularity(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """WafMetricsGranularity."""
 
     PT5_M = "PT5M"
     PT1_H = "PT1H"
     P1_D = "P1D"
 
 
-class WafMetricsResponseSeriesItemUnit(str, Enum, metaclass=CaseInsensitiveEnumMeta):
-    """WafMetricsResponseSeriesItemUnit."""
+class WafMetricsSeriesUnit(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """WafMetricsSeriesUnit."""
 
     COUNT = "count"
 
--- sdk/cdn/azure-mgmt-cdn/generated_samples/origin_groups_list_by_endpoint.py
+++ sdk/cdn/azure-mgmt-cdn/generated_samples/origin_groups_list_by_endpoint.py
@@ -14,9 +14,9 @@ from azure.mgmt.cdn import CdnManagementClient
     pip install azure-identity
     pip install azure-mgmt-cdn
 # USAGE
-    python custom_domains_enable_custom_https_using_your_own_certificate.py
+    python origin_groups_list_by_endpoint.py
 
-    Before run the sample, please set the values of the client ID, tenant ID and client secret 
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
     AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
     https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
@@ -29,15 +29,15 @@ def main():
         subscription_id="subid",
     )
 
-    response = client.custom_domains.begin_enable_custom_https(
+    response = client.origin_groups.list_by_endpoint(
         resource_group_name="RG",
         profile_name="profile1",
         endpoint_name="endpoint1",
-        custom_domain_name="www-someDomain-net",
-    ).result()
-    print(response)
+    )
+    for item in response:
+        print(item)
 
 
-# x-ms-original-file: specification/cdn/resource-manager/Microsoft.Cdn/stable/2021-06-01/examples/CustomDomains_EnableCustomHttpsUsingBYOC.json
+# x-ms-original-file: specification/cdn/resource-manager/Microsoft.Cdn/stable/2023-11-01/examples/OriginGroups_ListByEndpoint.json
 if __name__ == "__main__":
     main()
