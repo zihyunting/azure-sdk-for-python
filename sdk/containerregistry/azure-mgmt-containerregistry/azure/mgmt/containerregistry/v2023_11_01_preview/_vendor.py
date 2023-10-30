fd0564a0dd98b58a93ae19608712fc9e2cdb3459
SDKAuto <sdkautomation@microsoft.com>
2023-10-30 22:33:49 +0000

    CodeGen from PR 25918 in Azure/azure-rest-api-specs Raymondluo1 containerregistry microsoft.container registry 2023 11 01 preview (#25918)
    * Adds base for updating Microsoft.ContainerRegistry from version preview/2023-08-01-preview to version 2023-11-01-preview
    
    * Updates readme
    
    * Updates API version in new specs and examples
    
    * Added the metadataSearchEnabled property.
    
    * Fixed the tag format in readme
    
    * Updated examples for the metadata search
    
    * Fix format issue on the readme.
    
    * Change the metadataSearch to a Enum
    
    * Fixed the format issue.
    
    * Revert to the format before.
    
    * Fix the ENUM case issue.
    
    * Added the changes for the readme for sdks

--- sdk/containerregistry/azure-mgmt-containerregistry/azure/mgmt/containerregistry/v2023_11_01_preview/_vendor.py
+++ sdk/containerregistry/azure-mgmt-containerregistry/azure/mgmt/containerregistry/v2023_11_01_preview/_vendor.py
@@ -1,4 +1,3 @@
-# coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
@@ -6,5 +5,12 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._container_registry_management_client import ContainerRegistryManagementClient
-__all__ = ['ContainerRegistryManagementClient']
+from azure.core.pipeline.transport import HttpRequest
+
+
+def _convert_request(request, files=None):
+    data = request.content if not files else None
+    request = HttpRequest(method=request.method, url=request.url, headers=request.headers, data=data)
+    if files:
+        request.set_formdata_body(files)
+    return request
