ebed403c2b1bbff035bfe563a2caa35384a784b5
SDKAuto <sdkautomation@microsoft.com>
2023-11-01 20:10:42 +0000

    CodeGen from PR 26474 in Azure/azure-rest-api-specs Zhugexubin maps microsoft.maps 2023 08 01 preview new (#26474)
    * Adds base for updating Microsoft.Maps from version stable/2023-06-01 to version 2023-08-01-preview
    
    * Updates readme
    
    * Updates API version in new specs and examples
    
    * Add totalStorageUnitSizeInBytes and consumedStorageUnitSizeInBytes in creator properties
    
    ---------
    
    Co-authored-by: Xubin Zhuge <xuzhuge@microsoft.com>

--- sdk/maps/azure-mgmt-maps/generated_samples/list_accounts_by_resource_group.py
+++ sdk/maps/azure-mgmt-maps/generated_samples/list_accounts_by_resource_group.py
@@ -36,6 +36,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/maps/resource-manager/Microsoft.Maps/stable/2023-06-01/examples/ListAccountsByResourceGroup.json
+# x-ms-original-file: specification/maps/resource-manager/Microsoft.Maps/preview/2023-08-01-preview/examples/ListAccountsByResourceGroup.json
 if __name__ == "__main__":
     main()
