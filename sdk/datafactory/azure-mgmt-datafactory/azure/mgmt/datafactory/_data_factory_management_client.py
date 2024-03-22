6ce19b05189f17e5425fa8f65e222323bb7fcbf1
SDKAuto <sdkautomation@microsoft.com>
2024-03-22 02:54:05 +0000

    CodeGen from PR 28378 in Azure/azure-rest-api-specs Merge 7a0e6f11fe3fdebc363fe164f0892b4811448c37 into 8d1d3d29cdbe03f95a9b4c0ec6bb2221f1c44bde

--- sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/_data_factory_management_client.py
+++ sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/_data_factory_management_client.py
@@ -9,8 +9,10 @@
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
 
+from azure.core.pipeline import policies
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core.policies import ARMAutoResourceProviderRegistrationPolicy
 
 from . import models as _models
 from ._configuration import DataFactoryManagementClientConfiguration
@@ -31,7 +33,6 @@ from .operations import (
     LinkedServicesOperations,
     ManagedPrivateEndpointsOperations,
     ManagedVirtualNetworksOperations,
-    Operations,
     PipelineRunsOperations,
     PipelinesOperations,
     PrivateEndPointConnectionsOperations,
@@ -50,8 +51,6 @@ class DataFactoryManagementClient:  # pylint: disable=client-accepts-api-version
     """The Azure Data Factory V2 management API provides a RESTful set of web services that interact
     with Azure Data Factory V2 services.
 
-    :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.datafactory.operations.Operations
     :ivar factories: FactoriesOperations operations
     :vartype factories: azure.mgmt.datafactory.operations.FactoriesOperations
     :ivar exposure_control: ExposureControlOperations operations
@@ -129,13 +128,30 @@ class DataFactoryManagementClient:  # pylint: disable=client-accepts-api-version
         self._config = DataFactoryManagementClientConfiguration(
             credential=credential, subscription_id=subscription_id, **kwargs
         )
-        self._client: ARMPipelineClient = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        _policies = kwargs.pop("policies", None)
+        if _policies is None:
+            _policies = [
+                policies.RequestIdPolicy(**kwargs),
+                self._config.headers_policy,
+                self._config.user_agent_policy,
+                self._config.proxy_policy,
+                policies.ContentDecodePolicy(**kwargs),
+                ARMAutoResourceProviderRegistrationPolicy(),
+                self._config.redirect_policy,
+                self._config.retry_policy,
+                self._config.authentication_policy,
+                self._config.custom_hook_policy,
+                self._config.logging_policy,
+                policies.DistributedTracingPolicy(**kwargs),
+                policies.SensitiveHeaderCleanupPolicy(**kwargs) if self._config.redirect_policy else None,
+                self._config.http_logging_policy,
+            ]
+        self._client: ARMPipelineClient = ARMPipelineClient(base_url=base_url, policies=_policies, **kwargs)
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
         self.factories = FactoriesOperations(self._client, self._config, self._serialize, self._deserialize)
         self.exposure_control = ExposureControlOperations(
             self._client, self._config, self._serialize, self._deserialize
@@ -185,7 +201,7 @@ class DataFactoryManagementClient:  # pylint: disable=client-accepts-api-version
             self._client, self._config, self._serialize, self._deserialize
         )
 
-    def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
+    def _send_request(self, request: HttpRequest, *, stream: bool = False, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -205,7 +221,7 @@ class DataFactoryManagementClient:  # pylint: disable=client-accepts-api-version
 
         request_copy = deepcopy(request)
         request_copy.url = self._client.format_url(request_copy.url)
-        return self._client.send_request(request_copy, **kwargs)
+        return self._client.send_request(request_copy, stream=stream, **kwargs)  # type: ignore
 
     def close(self) -> None:
         self._client.close()
