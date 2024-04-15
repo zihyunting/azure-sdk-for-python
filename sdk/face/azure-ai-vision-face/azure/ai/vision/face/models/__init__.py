5bf38b3f06048138e74995fe231a164613e6fa73
Judith Ting <zihyunting@microsoft.com>
2024-04-15 09:03:05 +0800

    Fix SDK error

--- sdk/face/azure-ai-vision-face/azure/ai/vision/face/models/__init__.py
+++ sdk/face/azure-ai-vision-face/azure/ai/vision/face/models/__init__.py
@@ -35,8 +35,10 @@ from ._models import LivenessResponseBody
 from ._models import LivenessSession
 from ._models import LivenessSessionAuditEntry
 from ._models import LivenessSessionCreationContent
+from ._models import LivenessSessionCreationContentForMultipart
 from ._models import LivenessSessionCreationResult
 from ._models import LivenessSessionItem
+from ._models import LivenessSessionWithVerifyImageCreationContent
 from ._models import LivenessWithVerifyImage
 from ._models import LivenessWithVerifyOutputs
 from ._models import LivenessWithVerifySession
@@ -49,6 +51,7 @@ from ._models import SessionAuditEntryRequestInfo
 from ._models import SessionAuditEntryResponseInfo
 
 from ._enums import FaceDetectionModel
+from ._enums import FaceAttributeType
 from ._enums import FaceImageType
 from ._enums import FaceLivenessDecision
 from ._enums import FaceOperationState
@@ -93,8 +96,10 @@ __all__ = [
     "LivenessSession",
     "LivenessSessionAuditEntry",
     "LivenessSessionCreationContent",
+    "LivenessSessionCreationContentForMultipart",
     "LivenessSessionCreationResult",
     "LivenessSessionItem",
+    "LivenessSessionWithVerifyImageCreationContent",
     "LivenessWithVerifyImage",
     "LivenessWithVerifyOutputs",
     "LivenessWithVerifySession",
@@ -106,6 +111,7 @@ __all__ = [
     "SessionAuditEntryRequestInfo",
     "SessionAuditEntryResponseInfo",
     "FaceDetectionModel",
+    "FaceAttributeType",
     "FaceImageType",
     "FaceLivenessDecision",
     "FaceOperationState",
